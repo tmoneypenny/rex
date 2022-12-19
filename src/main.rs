2c94cdc329d1139af88007e24750df7e427ad70f
tmoneypenny <4460992+tmoneypenny@users.noreply.github.com>
2022-12-18 21:16:24 -0800

    pass argv & envp to fexecve
    This patch resolves passing args and envs to the executed ELF.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-use libc;
 use log::{debug, error};
 use std::io::prelude::*;
 use std::os::raw::{c_char, c_void};
 use std::{ffi, process};
 
-const ELF_NAME: &'static str = "REX"; // ELF FD name
+const ELF_NAME: &str = "REX"; // ELF FD name
 
 fn main() -> Result<(), i8> {
     env_logger::init();
-    debug!("Hello World");
 
     let fd_name = ffi::CString::new(ELF_NAME).expect("c_char couldn't be created");
 
+    // Create in-memory file descriptor
     let mdf = unsafe {
         match libc::memfd_create(fd_name.as_ptr() as *const c_char, 0) {
             -1 => {
@@ -25,7 +24,7 @@ fn main() -> Result<(), i8> {
         }
     };
 
-    // read from stdin
+    // Read ELF from stdin
     let mut buffer = Vec::new();
     let stdin = std::io::stdin();
     {
@@ -39,7 +38,7 @@ fn main() -> Result<(), i8> {
         }
     }
 
-    // writeall to mdf
+    // Write-all buf to memfd
     unsafe {
         let written = libc::write(
             mdf as i32,
@@ -49,16 +48,29 @@ fn main() -> Result<(), i8> {
         debug!("Wrote {} bytes", written);
     };
 
-    // TODO: fix argv + envp
-    let args = [""];
-    let env = [""];
+    // Parse argv and envp
+    let args = std::env::args()
+        .map(|arg| ffi::CString::new(arg).unwrap())
+        .collect::<Vec<ffi::CString>>();
 
+    let mut c_args = args
+        .iter()
+        .map(|arg| arg.as_ptr())
+        .collect::<Vec<*const c_char>>();
+    c_args.push(std::ptr::null());
+
+    let envp = std::env::vars()
+        .map(|(k, v)| ffi::CString::new(format!("{}={}", k, v)).unwrap())
+        .collect::<Vec<ffi::CString>>();
+    let mut c_envp = envp
+        .iter()
+        .map(|envp| envp.as_ptr())
+        .collect::<Vec<*const c_char>>();
+    c_envp.push(std::ptr::null());
+
+    // Execute in-memory file descriptor
     unsafe {
-        match libc::fexecve(
-            mdf as i32,
-            args.as_ptr() as *const *const c_char,
-            env.as_ptr() as *const *const c_char,
-        ) {
+        match libc::fexecve(mdf as i32, c_args.as_ptr(), c_envp.as_ptr()) {
             -1 => {
                 error!("failed to execute in-memory file descriptor");
                 process::exit(1)
